a9c006039427107914aae15f76e5aa5b683ded97
Håvard Dahl <haavda@Havard-sin-MacBook-Pro.local>
2024-10-21 20:26:59 +0200

    Fixed critical issue with storage account

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -5,6 +5,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 }
 
 #Creating the Storage Container
